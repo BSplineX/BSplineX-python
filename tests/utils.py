bcd6b7dc9193b8819cbe11ea1597ab596fcca591
Sebastiano Taddei <61469576+SebastianoTaddei@users.noreply.github.com>
2025-04-25 16:06:25 +0200

    Feature/1 d derivatives (#3)
    * Add example to compute derivatives
    
    * Updated submodule
    
    * Updated factory functions to new names
    
    * Remove tests since they should be updated
    ---------
    
    Co-authored-by: Gabriele Masina <gabriele.masina@virgilio.it>

--- tests/utils.py
+++ tests/utils.py
@@ -1,18 +1,5 @@
 import numpy as np
 
-
-def num_control_points_open(n_knots, degree):
-    return n_knots - degree - 1
-
-
-def num_control_points_clamped(n_knots, degree):
-    return n_knots + degree - 1
-
-
-def num_control_points_periodic(n_knots, degree):
-    return n_knots - 1
-
-
 def x_values_open(knots, degree):
     return np.linspace(
         knots[degree],
@@ -36,3 +23,5 @@ def x_values_periodic(knots, degree):
         knots[-degree - 1] + 3 * period,
         3000,
     )
+
+
